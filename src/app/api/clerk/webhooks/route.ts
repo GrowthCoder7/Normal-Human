3a4cf63f3c56a152c719596f9e43842d3713d9b8
Prince Kumar <pk7411956419@gmail.com>
2025-11-09 15:29:21 +0530

    Added auth routes with clerk, webhook clerk&db setup done

---
+++ src/app/api/clerk/webhooks/route.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { db } from "@/server/db"
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+ export const POST = async (req: Request) => {
+    const {data} = await req.json()
+    const firstName = data.first_name;
+    const lastName= data.last_name;
+    const email = 'johnDoe@gmail.com'
+    const imageUrl = data.image_url;
+
+    await db.user.create({
+        data: {
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            imageUrl:imageUrl
+        }
+    })
+
+    console.log("clerk webhook",data)
+    return new Response('User Created!',{status:200})
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import "@/styles/globals.css";
 import {
   ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
 } from '@clerk/nextjs'
 import { type Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
 
@@ -20,47 +13,23 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
-
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
 });
 
-
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
-        </body>
-      </html>
+    <html lang="en" className={`${geist.variable}`}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
     </ClerkProvider>
-  )
+  );
 }
+
+
